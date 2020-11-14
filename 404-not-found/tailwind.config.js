fc16882323bc5c37cdd52add181bd58a8ac3f984
Armando Guarino <arm.guarino@gmail.com>
2020-11-14 22:00:08 +0100

    404 not found

---
+++ 404-not-found/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      'mono': ['Space Mono', 'monospace'],
+      'sans': ['Inconsolata', 'monospace']
+    },
+    container: {
+      center: true,
+      padding: '2rem'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
